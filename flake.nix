4573019b61820a601dce02f257208ab9594fa86b
awesomearvinder <ArvinderDhan@gmail.com>
2021-05-20 10:38:07 -0500

    better flake setup

--- flake.nix
+++ flake.nix
@@ -1,20 +1,36 @@
 {
   description = "A very basic flake";
-  inputs.pkgs_url.url = "github:NixOS/nixpkgs/nixos-20.09";
-
-  outputs = { self, pkgs_url }:
-    let pkgs = import pkgs_url {system="x86_64-linux";};
-    in
-  {
-    devShell."x86_64-linux" = pkgs.mkShell {
-      nativeBuildInputs = [
-        pkgs.cargo
-        pkgs.rustc
-        pkgs.rustfmt
-        pkgs.openssl
-        pkgs.pkg-config
-      ];
-      buildInputs = [ pkgs.rust-analyzer ];
-    };
+  inputs = {
+    pkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    naersk.url = "github:nmattia/naersk";
+    utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, pkgs, unstable, naersk, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgsLegacyPackages = pkgs.legacyPackages."${system}";
+        unstableLegacyPackages = unstable.legacyPackages."${system}";
+        compileDeps = [ unstableLegacyPackages.cargo unstableLegacyPackages.rustc ];
+        runtimeDeps = [ 
+          pkgsLegacyPackages.openssl 
+          pkgsLegacyPackages.pkg-config
+        ];
+        RUST_SRC_PATH =
+          "${unstable.rust.packages.stable.rustPlatform.rustLibSrc}";
+      in {
+        defaultPackage = naersk.lib."${system}".buildPackage {
+          pname = "website-backend";
+          root = ./.;
+          buildInputs = compileDeps;
+          nativeBuildInputs = runtimeDeps;
+        };
+
+        devShell."${system}" = pkgsLegacyPackages.mkShell {
+          nativeBuildInputs = compileDeps ++ [ pkgsLegacyPackages.rust-analyzer ];
+          buildInputs = runtimeDeps;
+          inherit RUST_SRC_PATH;
+        };
+      });
 }
